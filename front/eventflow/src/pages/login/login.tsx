7784a2ee0807c567efc487fbc305089df66349fc
pedroedu06 <pedro.nascimento182501@gmail.com>
2025-08-11 12:30:32 -0300

    tela de login pacialmente finalizada e iniciando a integracao de dados para um banco de dados

--- front/eventflow/src/pages/login/login.tsx
+++ front/eventflow/src/pages/login/login.tsx
@@ -1,9 +1,35 @@
 import type React from "react";
+import { SlArrowLeft } from "react-icons/sl";
+import { useState } from 'react';
 import "./login.css";
 
+
 const Login: React.FC = () => {
+    
+    const backToHome = () => {
+        window.location.href = "/";
+    }
+
     return (
-        <div><p>teste</p></div>
+        <div>
+            <nav className="navbar-register">
+                    <img src="" alt="logo do site" className="logo-register"/>
+                    <span onClick={backToHome} className="back-button"><SlArrowLeft /> VOLTAR </span>
+            </nav>
+        <div className="login-container">
+            <div className="loginConteiner-Form">
+            <form>
+                <input type="text" placeholder="Email ou Username" required/>
+                <input type="password" placeholder="Senha" required />
+                <div className="abasdeesqueceuecriar">
+                    <a href="">Esqueceu a senha?</a>
+                    <a href="/register ">Criar uma conta</a>
+                </div>
+                <button type="submit">Entrar</button>
+            </form>
+            </div>
+        </div>
+        </div>
     )
 }
 
--- front/eventflow/src/pages/register/register.tsx
+++ front/eventflow/src/pages/register/register.tsx
@@ -1,33 +1,56 @@
 import "./register.css";
 import { SlArrowLeft } from "react-icons/sl";
+import React, { useState } from 'react';
+import { set, useForm } from "react-hook-form"
 
 const Register: React.FC = () => {
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [birthdate, setBirthdate] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [error, setError] = useState("")
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (password !== confirmPassword) {
+            setError("As senhas não coincidem")
+            return;
+        }
+        setError("");
+
+        setEmail("");
+        setUsername("");
+        setBirthdate("");
+        setPassword("");
+        setConfirmPassword("");
+    }
+
+     const backToHome = () => {
+        window.location.href = "/";
+    }
+
    return (
     <div>
         <nav className="navbar-register">
         <img src="" alt="logo do site" className="logo-register"/>
-        <span role="button"
-        className="back-button"
-        tabIndex={0}
-        onClick={() => {
-            addEventListener("click", () =>{
-                window.location.href = "/";
-            })
-        }}
-        ><SlArrowLeft /> VOLTAR </span>
+        <span className="back-button" onClick={backToHome}><SlArrowLeft /> VOLTAR </span>
         </nav>
     <div className="register-container">
-            <form className="register-form">
+            <form className="register-form" onSubmit={handleSubmit}>
                 <label htmlFor="email">Email</label>
-                <input type="email" placeholder="Insira um Email" required/>
+                <input type="email" placeholder="Insira um Email" required onChange={(e) => setEmail(e.target.value)}/>
                 <label htmlFor="username">Nome de usuário</label>
-                <input type="text" placeholder="Insira um nome de usuário" required/>
+                <input type="text" placeholder="Insira um nome de usuário" required onChange={(e) => setUsername(e.target.value)}/>
                 <label htmlFor="birthdate">Data de nascimento</label>
-                <input type="date" placeholder="Insira sua data de nascimento" className="input-data" required/>
+                <input type="date" placeholder="Insira sua data de nascimento" className="input-data" required onChange={(e) => setBirthdate(e.target.value)}/>
                 <label htmlFor="password">Senha</label>
-                <input type="password" placeholder="Insira sua senha" required/>
+                <input type="password" placeholder="Insira sua senha" required onChange={(e) => setPassword(e.target.value)}/>
                 <label htmlFor="confirm-password">Confirme a senha</label>
-                <input type="password" placeholder="Confirme a senha" required/>
+                <input type="password" placeholder="Confirme a senha" required onChange={(e) => setConfirmPassword(e.target.value)}/>
+                {error && <span style={{ color: 'red' }}>{error}</span>}
+                
                 <button type="submit">Criar usuario</button>
             </form>
     </div>
